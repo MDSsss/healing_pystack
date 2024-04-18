52be2c53872531d6d27a73065b7092debf2207f6
MDSsss <Mauricio_domingo.09@hotmail.com>
2024-04-18 17:29:29 -0300

    Começar fazer logout

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.messages import constants
 from django.contrib import messages
+from django.contrib import auth
+
+
 
 # Create your views here.
 
@@ -37,3 +40,19 @@ def cadastro(request):
         )
 
         return redirect('/usuarios/login')
+    
+def login_view(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')    
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+
+        user = auth.authenticate(request, username=username, password=senha)
+
+        if user:
+            auth.login(request, user)
+            return redirect('/pacientes/home') #por enquanto vai dar erro
+        messages.add_message(request, constants.ERROR, 'Usuário ou senha inválidos.')
+        return redirect('/usuarios/login')
+    
